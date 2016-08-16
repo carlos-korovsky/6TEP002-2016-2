a4cc67ffae1d7d7aba996d304f6b679333e032fc
Carlos Alberto Cipriano Korovsky <carlos.korovsky@udesc.br>
2016-08-16 19:06:12 -0300

    Implementação do login

--- Exemplo01/app/src/main/java/br/udesc/ceplan/a3tep002/exemplo01/MainActivity.java
+++ Exemplo01/app/src/main/java/br/udesc/ceplan/a3tep002/exemplo01/MainActivity.java
@@ -2,6 +2,10 @@ package br.udesc.ceplan.a3tep002.exemplo01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                TextView txLogin = (TextView) findViewById(R.id.txLogin);
+                TextView txPassword = (TextView) findViewById(R.id.txPassword);
+                String login = txLogin.getText().toString();
+                String password = txPassword.getText().toString();
+                if (login.equals("demo") && (password.equals("demo"))) {
+                    alerta("Bem vindo! Login realizado com sucesso");
+                } else {
+                    alerta("Login ou senha inválidos");
+                }
+            }
+        });
+    }
+
+    private void alerta(String mensagem) {
+        Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show();
     }
 }
