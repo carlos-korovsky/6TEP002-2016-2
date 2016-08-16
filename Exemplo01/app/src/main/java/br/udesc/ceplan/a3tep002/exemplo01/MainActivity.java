846e0699378de88dac49b66cee9b39d4f0b0f8d5
Carlos Alberto Cipriano Korovsky <carlos.korovsky@udesc.br>
2016-08-16 13:14:59 -0300

    Importação inicial

---
+++ Exemplo01/app/src/main/java/br/udesc/ceplan/a3tep002/exemplo01/MainActivity.java
@@ -0,0 +1,13 @@
+package br.udesc.ceplan.a3tep002.exemplo01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
