6d2234d0f5df32fd6b3619821ea4615a73fc3b2a
Carlos Alberto Cipriano Korovsky <carlos.korovsky@udesc.br>
2016-11-01 18:04:56 -0200

    Implementado método para remover uma tarefa

--- ExemploMVC/app/src/main/java/br/udesc/ceplan/tep/exemplomvc/TarefaDetailActivity.java
+++ ExemploMVC/app/src/main/java/br/udesc/ceplan/tep/exemplomvc/TarefaDetailActivity.java
@@ -3,9 +3,13 @@ package br.udesc.ceplan.tep.exemplomvc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import br.udesc.ceplan.tep.exemplomvc.model.Tarefa;
 
 public class TarefaDetailActivity extends AppCompatActivity {
 
@@ -13,19 +17,25 @@ public class TarefaDetailActivity extends AppCompatActivity {
     public static final String TAREFA_NAME = "br.udesc.ceplan.tep.exemplomvc.TAREFA_NAME";
     public static final String TAREFA_DESC = "br.udesc.ceplan.tep.exemplomvc.TAREFA_DESC";
 
+    private Long id;
+    private ApplicationController aController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarefa_detail);
 
+        this.aController = (ApplicationController) getApplicationContext();
+
         TextView txId = (TextView) this.findViewById(R.id.txTarefaDetailId);
         TextView txName = (TextView) this.findViewById(R.id.txTarefaDetailName);
         TextView txDesc = (TextView) this.findViewById(R.id.txTarefaDetailDesc);
         Button btDelete = (Button) this.findViewById(R.id.btTarefaDetailDelete);
+        btDelete.setOnClickListener(handleDeletarTarefaEvent);
 
         Intent intent = getIntent();
 
-        Long id = intent.getLongExtra(TAREFA_ID, -1);
+        id = intent.getLongExtra(TAREFA_ID, -1);
         String name = intent.getStringExtra(TAREFA_NAME);
         String desc = intent.getStringExtra(TAREFA_DESC);
 
@@ -34,4 +44,15 @@ public class TarefaDetailActivity extends AppCompatActivity {
         txDesc.setText(desc);
 
     }
+
+    private final View.OnClickListener handleDeletarTarefaEvent =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(final View view) {
+                    Log.d(MainActivity.APP_TAG, "botão deletar tarefa acionado");
+                    Long _id = TarefaDetailActivity.this.id;
+                    TarefaDetailActivity.this.aController.getDaoFactorySQLite().getTarefaDao().delete(_id);
+
+                }
+            };
 }
